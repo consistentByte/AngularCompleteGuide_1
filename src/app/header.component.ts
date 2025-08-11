6fe22e078e889f76e5d1b42b6cd37d6ebe0bb226
Saurabh Pandey <consistentByte@gmail.com>
2025-08-11 13:36:12 +0530

    15 16 =>imports, styles.css, assets Using custom component, Styling the Header Component & Adding An Image

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,11 +1,11 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-header',
-    // template: '<h1>Hello World</h1>',
-    templateUrl: './header.component.html',
-    standalone: true
+  selector: 'app-header',
+  // template: '<h1>Hello World</h1>',
+  templateUrl: './header.component.html',
+  standalone: true,
+  // styles: ['h1 {color: red }']
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
