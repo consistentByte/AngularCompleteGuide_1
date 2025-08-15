f943e3520559ee0b1316c66c6150a856ffb7478c
Saurabh Pandey <consistentByte@gmail.com>
2025-08-15 23:18:42 +0530

    35 => output() function signal

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -6,6 +6,7 @@ import {
   input,
   Output,
   EventEmitter,
+  output,
 } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -20,7 +21,8 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
+  // @Output() select = new EventEmitter();
+  select = output<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
