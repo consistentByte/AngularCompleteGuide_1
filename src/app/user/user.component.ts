42a3b50a9ceb1f74bfb1d350f421657c3329a4c1
Saurabh Pandey <consistentByte@gmail.com>
2025-08-15 23:22:39 +0530

    36 => adding extra info (type safety) to EventEmitter

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -6,7 +6,6 @@ import {
   input,
   Output,
   EventEmitter,
-  output,
 } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -21,8 +20,7 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  // @Output() select = new EventEmitter();
-  select = output<string>();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
