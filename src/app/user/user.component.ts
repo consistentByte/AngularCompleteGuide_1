ce6c770e083ee72cd7345491299d39a1f9a8728a
Saurabh Pandey <consistentByte@gmail.com>
2025-08-15 21:35:14 +0530

    33

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,16 +9,12 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  avatar = input.required<string>();
-  name = input.required<string>();
-  imagePath = computed(() => 'assets/users/' + this.avatar());
-
-  // get imagePath() {
-  //   return 'assets/users/' + this.avatar;
-  // }
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() {}
 }
