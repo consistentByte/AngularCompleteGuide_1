9878cc6dee6a78d9455f9fb89d58f474a148a279
Saurabh Pandey <consistentByte@gmail.com>
2025-08-15 23:41:31 +0530

    37 => Creating tasks component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,24 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   //exposing imported data to component template
   users = DUMMY_USERS;
+  selectedUserId = DUMMY_USERS[0].id;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
 
   onSelectUser(id: string) {
-    console.log(id);
+    this.selectedUserId = id;
   }
 }
