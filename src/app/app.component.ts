204cb7c46148fa2881e9975c1f64c9c54867b5f2
Saurabh Pandey <consistentByte@gmail.com>
2025-08-17 17:54:12 +0530

    68 => Bootstrapping app with angular modules, platformUserDynamic().bootstrapModule(), bootstrap: []

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,6 @@ import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
