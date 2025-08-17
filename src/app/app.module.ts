c95e39cf6f988151a236da31468aa934f9669b00
Saurabh Pandey <consistentByte@gmail.com>
2025-08-17 18:02:39 +0530

    69 => Declaring & Using Components, imports array, browserModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
-  declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent], // for importing non-standalone components
+  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent], // for importing other modules or importing any standalone component.
+  bootstrap: [AppComponent], // if this module is used to bootstrap application then which components must be used to bootstrap,
 })
 export class AppModule {}
