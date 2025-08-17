5029d5b5ebd15ed9c37288d146bbbe806de1f4ec
Saurabh Pandey <consistentByte@gmail.com>
2025-08-17 20:20:09 +0530

    73 => Creating TasksModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,11 @@ import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { TaskComponent } from './tasks/task/task.component';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    UserComponent,
-    TasksComponent,
-    NewTaskComponent,
-    TaskComponent,
-  ], // for importing non-standalone components
-  imports: [BrowserModule, FormsModule, SharedModule], // for importing other modules or importing any standalone component.
+  declarations: [AppComponent, HeaderComponent, UserComponent], // for importing non-standalone components
+  imports: [BrowserModule, SharedModule, TasksModule], // for importing other modules or importing any standalone component.
   bootstrap: [AppComponent], // if this module is used to bootstrap application then which components must be used to bootstrap,
 })
 export class AppModule {}
