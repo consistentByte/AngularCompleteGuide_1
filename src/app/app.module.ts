9d1402a0342069813e0f2259da9d03dac3fc7eed
Saurabh Pandey <consistentByte@gmail.com>
2025-08-17 19:20:00 +0530

    71 => migrating all components to use modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { CardComponent } from './shared/card/card.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { TaskComponent } from './tasks/task/task.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent], // for importing non-standalone components
-  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent], // for importing other modules or importing any standalone component.
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    CardComponent,
+    TasksComponent,
+    NewTaskComponent,
+    TaskComponent,
+  ], // for importing non-standalone components
+  imports: [BrowserModule, FormsModule], // for importing other modules or importing any standalone component.
   bootstrap: [AppComponent], // if this module is used to bootstrap application then which components must be used to bootstrap,
 })
 export class AppModule {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  // template: '<h1>Hello World</h1>',
   templateUrl: './header.component.html',
-  standalone: true,
-  // styles: ['h1 {color: red }']
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {}
