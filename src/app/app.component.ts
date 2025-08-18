bd9a53d7207219e20fae6755b5ce09f4c07bf4d4
Saurabh Pandey <consistentByte@gmail.com>
2025-08-18 19:14:40 +0530

    74 => init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,8 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from './tasks/tasks.component';
+
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  //exposing imported data to component template
-  users = DUMMY_USERS;
-  selectedUserId?: string;
-
-  get selectedUser() {
-    return this.users.find((user) => user.id === this.selectedUserId);
-  }
-
-  onSelectUser(id: string) {
-    this.selectedUserId = id;
-  }
-}
+export class AppComponent {}
--- src/app/shared/shared.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CardComponent } from './card/card.component';
-
-@NgModule({
-  declarations: [CardComponent],
-  exports: [CardComponent], // Components that must not only be available inside shared module but also to all other modules that import the shared module
-})
-export class SharedModule {}
