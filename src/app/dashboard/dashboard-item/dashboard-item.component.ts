738de40b5044d0985ad6b91fb4deb2f8dcc30f92
Saurabh Pandey <consistentByte@gmail.com>
2025-08-21 11:22:20 +0530

    100 => Component Inputs for DashBoardItem , both signal and decorator approach

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
   standalone: true,
   imports: [],
   templateUrl: './dashboard-item.component.html',
-  styleUrl: './dashboard-item.component.css'
+  styleUrl: './dashboard-item.component.css',
 })
 export class DashboardItemComponent {
+  // @Input({ required: true }) image!: { src: ''; alt: '' };
+  // @Input({required: true}) title!: string;
 
+  image = input.required<{ src: ''; alt: '' }>();
+  title = input.required<''>();
 }
