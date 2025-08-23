4076ee0251b0e7e13417055e1871059b62a80bd8
Saurabh Pandey <consistentByte@gmail.com>
2025-08-23 13:25:58 +0530

    116 => Interacting with host elements using @HostBinding & @HostListener

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,10 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  HostListener,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -9,8 +15,24 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
   host: {
     class: 'control',
+    '(click)': 'onhostClick()',
   },
 })
 export class ControlComponent {
+  // Alternate to setting host property of Component decorator to set class to host element.
+  // @HostBinding('class') className = 'control';
+  // HostBinding not preffered now, its better to use host property instead.
+
   label = input.required<string>();
+
+  onhostClick() {
+    console.log('Host element clicked');
+  }
+
+  // @HostListener('click') onhostClickByHostListener() {
+  //   console.log('Host element clicked HostListener');
+  // }
 }
+
+//HostBinding => binding a property to host element with a value.
+//HostListener => adding an event listener to host element.
