0754f2e73be0563766a6dfa57ce33d143345e59e
Saurabh Pandey <consistentByte@gmail.com>
2025-08-24 13:43:48 +0530

    131 => Getting access to template variable using ViewChild, ElementRef, HTMLFormElement, form.reset()

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { FormsModule } from '@angular/forms';
@@ -11,8 +11,13 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
+  // @ViewChild(ButtonComponent) form!: HTMLFormElement; // if we put a class name as selector ViewChild will try to find an instance of buttoncomponent and will store that in form property
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+
   onSubmit(title: string, ticketText: string) {
     console.dir(title);
     console.dir(ticketText);
+    // form.reset();
+    this.form?.nativeElement.reset();
   }
 }
