4f250fd258639639567337b11ee616329526aafa
Saurabh Pandey <consistentByte@gmail.com>
2025-08-24 14:48:58 +0530

    134 => @ViewChild and viewChild() , @ContentChild and contentChild() accessibility difference in ngOnInit, and importance of ngAfterViewInit, ngAfterContentInit

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, viewChild, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild, ViewChildren } from '@angular/core';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { FormsModule } from '@angular/forms';
@@ -10,10 +10,10 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements OnInit, AfterViewInit{
   // @ViewChild(ButtonComponent) form!: HTMLFormElement; // if we put a class name as selector ViewChild will try to find an instance of buttoncomponent and will store that in form property
-  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
-  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  // private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
   // to select multiple children use ViewChildren
   // @ViewChildren(ButtonComponent) buttonComponent?: ButtonComponent;
@@ -22,6 +22,20 @@ export class NewTicketComponent {
     console.dir(title);
     console.dir(ticketText);
     // form.reset();
-    this.form().nativeElement.reset();
+    this.form?.nativeElement.reset();
+  }
+
+  ngOnInit(): void {
+    console.log('ON INIT');
+    console.log(this.form?.nativeElement);
+  }
+  ngAfterViewInit(): void {
+    // here we are guaranteed to have access viewChild elements since view is initialized
+    console.log('AFTER VIEW INIT');
+    console.log(this.form?.nativeElement);
   }
 }
+
+// if we use signal viewChild function we may get access to viewChild elements in ngOnInit, 
+// but if we are using @ViewChild decorator, we won't get access to viewChild elements in ngOnInit, therefore ngAfterViewInit becomes imp there.
+// same thing happens for @ContentChild and contentChild signal function with respect to ngOnInit and ngAfterContentInit
