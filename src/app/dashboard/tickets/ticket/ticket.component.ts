7803fa94311350bf14b63e61f0fbfc14182ce8b9
Saurabh Pandey <consistentByte@gmail.com>
2025-08-26 00:24:19 +0530

    142 => updating signal values other than using set, update() =>Update the value of the signal based on its current value, and notify any dependents.

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { type Ticket } from '../ticket.model';
 
 @Component({
@@ -10,4 +10,12 @@ import { type Ticket } from '../ticket.model';
 })
 export class TicketComponent {
   data = input.required<Ticket>();
+  detailsVisible = signal(false);
+
+  onToggleDetails() {
+    // this.detailsVisible.set(!this.detailsVisible()); //here it won't setup a subscription and only read it once
+    //Alternate way to set, update(), only diff is update expects a function and passes the current value of signal as a parameter to it.
+    //update => Update the value of the signal based on its current value, and notify any dependents.
+    this.detailsVisible.update((wasVisible) => !wasVisible)
+  }
 }
