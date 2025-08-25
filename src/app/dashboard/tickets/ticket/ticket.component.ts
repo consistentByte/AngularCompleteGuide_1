7a3d8174c7de60937f51393ed8b1bf9c0c532622
Saurabh Pandey <consistentByte@gmail.com>
2025-08-26 00:39:32 +0530

    144 => Config objects in inputs and outputs, alias, transform

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -9,6 +9,12 @@ import { type Ticket } from '../ticket.model';
   styleUrl: './ticket.component.css',
 })
 export class TicketComponent {
+  // @Input({}) in decorator config object is passed directly in decorator.
+  // ticket = input.required<Ticket>({alias: 'data'}); // if we didn't put required then we will have to put a default value then config object will be the second parameter.
+  // ticket = input.required<Ticket>({transform: () => {}}); // takes in the input value and return the transform value.
+  //Similarly we can pass config object to output() and @Output, we can pass alias as we did in input. 
+
+  
   data = input.required<Ticket>();
   detailsVisible = signal(false);
   close = output();
