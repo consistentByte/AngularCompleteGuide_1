115748ae2fc22df506bc3f459435257dcbeb5435
Saurabh Pandey <consistentByte@gmail.com>
2025-08-26 13:09:41 +0530

    146 => Setting up custom two way binding, @Input, @Output, size, sizeChange, [(size)] naming pattern is imp for angular to know we are setting up custom two way binding for it to handle things for us.

--- src/app/rect/rect.component.ts
+++ src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, model, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,8 +9,14 @@ import { Component } from '@angular/core';
 })
 export class RectComponent {
   // Todo: Implement custom two-way binding
-
+  
+  //We can implement custom two-way binding using Input and Output decorators or their signal functions.
+  @Input({required: true}) size!: {width: string, height: string};
+  @Output() sizeChange = new EventEmitter<{width: string, height: string}>();
+  
+  //Name convention is neccessary for angular to understand that we are using size for two-way binding, so it does all heavy lifting for us.
   onReset() {
     // ...
+    this.sizeChange.emit({width: '200', height: '100'});
   }
 }
