9d7510a1d875f93d031524ffa09753adb0d6a262
Saurabh Pandey <consistentByte@gmail.com>
2025-08-26 13:50:12 +0530

    147 => easier way for two-way binding, model() function

--- src/app/rect/rect.component.ts
+++ src/app/rect/rect.component.ts
@@ -11,12 +11,23 @@ export class RectComponent {
   // Todo: Implement custom two-way binding
   
   //We can implement custom two-way binding using Input and Output decorators or their signal functions.
-  @Input({required: true}) size!: {width: string, height: string};
-  @Output() sizeChange = new EventEmitter<{width: string, height: string}>();
+  // @Input({required: true}) size!: {width: string, height: string};
+  // @Output() sizeChange = new EventEmitter<{width: string, height: string}>();
   
   //Name convention is neccessary for angular to understand that we are using size for two-way binding, so it does all heavy lifting for us.
+  
+  // Alternate way for two-way binding => model() function => from angular 17
+  // When using model function no need for seaprate Input and Output.
+
+  size = model.required<{width: string, height: string}>();
+
   onReset() {
     // ...
-    this.sizeChange.emit({width: '200', height: '100'});
+    //Instead of emitting something we simply update the signal like we always update a signal.
+    //And the component where we provide its value will be notified.
+    
+    // this.size.emit({width: '200', height: '100'});
+
+    this.size.set({width: '200', height: '100'});
   }
 }
