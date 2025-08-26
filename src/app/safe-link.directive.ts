f5a93f7d7e2ce4bee58e48792dba1746841b5ead
Saurabh Pandey <consistentByte@gmail.com>
2025-08-26 16:04:52 +0530

    154 => Using Attribute Directives To Change Element Behavior, attaching click listener to app-safe-link directive,

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -3,9 +3,20 @@ import { Directive } from "@angular/core";
 @Directive({
     selector: 'a[app-safe-Link]',
     standalone: true,
+    host: {
+        '(click)': 'onConfirmLeavePage($event)',
+    }
 })
 export class SafeLinkDirective {
     constructor(){
         console.log('Safe Link Directive is active!')
+    }
+
+    onConfirmLeavePage(event: MouseEvent) {
+        const wantsToLeave = window.confirm('Do you want to leave the app?');
+        if(wantsToLeave) {
+            return;
+        }
+        event.preventDefault(); // cancel the naviagation click
     }
 }
