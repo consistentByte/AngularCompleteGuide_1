22c193c51042b773d928f3bbbf654dd253c322c8
Saurabh Pandey <consistentByte@gmail.com>
2025-08-27 13:44:08 +0530

    156 => Directives and Dependency injection, injecting host element (Anchor Element) in directive and using that to get the href attribute and add queryParam.

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, input } from "@angular/core";
+import { Directive, ElementRef, inject, input } from "@angular/core";
 
 @Directive({
     selector: 'a[app-safe-Link]',
@@ -8,7 +8,12 @@ import { Directive, input } from "@angular/core";
     }
 })
 export class SafeLinkDirective {
-    queryParams = input<string>('myapp', {alias: 'app-safe-link'});
+    queryParams = input<string>('myapp', {alias: 'app-safe-Link'});
+    private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef); 
+    //Here we are letting angular know the return type, 
+    // inject(ElementRef) lets us access host element in ts file programmatically, 
+    // <ElementRef<HTMLAnchorElement>> here we are passing the expected return type, ElementRef is just a wrapper around the actual return element
+    // Since we knw we use this directive on only Anchor Element so, host element will be HTMLAnchorElement.
 
     constructor(){
         console.log('Safe Link Directive is active!')
@@ -17,9 +22,11 @@ export class SafeLinkDirective {
     onConfirmLeavePage(event: MouseEvent) {
         const wantsToLeave = window.confirm('Do you want to leave the app?');
         if(wantsToLeave) {
-            const address = (event.target as HTMLAnchorElement).href;
-            (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParams();            
+            // const address = (event.target as HTMLAnchorElement).href;
+            // (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParams();            
             
+            const address = this.hostElementRef.nativeElement.href;
+            this.hostElementRef.nativeElement.href = address + '?from=' + this.queryParams();
             return;
         }
         event.preventDefault(); // cancel the naviagation click
