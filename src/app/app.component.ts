7cec867cb34142923ec242c712f46e5bf2c2b7e9
Saurabh Pandey <consistentByte@gmail.com>
2025-08-27 20:22:53 +0530

    163 => Using Built-in pipes, DatePipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [DatePipe],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
