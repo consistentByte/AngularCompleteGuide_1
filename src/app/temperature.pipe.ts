b89c7bdba423da5f2f8eb0e74e66df5e604663d3
Saurabh Pandey <consistentByte@gmail.com>
2025-08-29 14:48:28 +0530

    167 => Accepting parameters in custom Pipe

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -1,20 +1,34 @@
-import { Pipe, PipeTransform } from "@angular/core";
+import { output, Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
     name: 'temp',
     standalone: true
 })
 export class TemperaturePipe implements PipeTransform{
-    transform(value: string | number, ...args:any[]) {
+    transform(value: string | number, inputType: 'cel' | 'fah', outputType?: 'cel' | 'fah') {
         let val: number;
         if(typeof value === 'string') {
             val = parseFloat(value);
         } else {
             val = value;
         }
-        const outputTemp = val*(9/5)+32;
+        let outputTemp:number;
+        if(inputType === 'cel' && outputType === 'fah') {
+            outputTemp = val*(9/5)+32;
+        } else if(inputType === 'fah' && outputType === 'cel') {
+            outputTemp = (val-32)*(5/9);
+        } else {
+            outputTemp = val;
+        }
+        let symbol: '°F' | '°C';
+        
+        if(!outputType) {
+            symbol = inputType === 'cel' ? '°C' : '°F';
+        } else{
+            symbol = outputType === 'cel' ? '°C' : '°F';
+        }
 
-        return `${outputTemp} °F`;
+        return `${outputTemp} ${symbol}`;
     }
     //add PipeTransform to force us write the transform method.
     //All pipe classes need a transform method,and this transform method is executed by angular when we use this pipe in a tempelate.
