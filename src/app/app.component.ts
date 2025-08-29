05edf4ed731ff5f5bbbc809eadbf79ac80b71c56
Saurabh Pandey <consistentByte@gmail.com>
2025-08-29 15:45:49 +0530

    169 => Building a pipe that sorts items, using pipe in @for, sort pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { DatePipe, DecimalPipe } from '@angular/common';
 import { TemperaturePipe } from './temperature.pipe';
+import { SortPipe } from './sort.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DatePipe, DecimalPipe, TemperaturePipe],
+  imports: [DatePipe, DecimalPipe, TemperaturePipe, SortPipe],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
