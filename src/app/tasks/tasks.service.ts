6b21d28e3f5dc54a70eee3c4284838f2acf156b1
Saurabh Pandey <consistentByte@gmail.com>
2025-09-03 00:56:25 +0530

    184 => Injecting Services into services

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
-import { Injectable, signal } from "@angular/core";
+import { inject, Injectable, signal } from "@angular/core";
 import { Task, TaskStatus } from "./task.model";
+import { LoggingService } from "../logging.service";
 
 // providedIn: 'root', to make it availble anywhere inside the application
 @Injectable({
@@ -8,6 +9,8 @@ import { Task, TaskStatus } from "./task.model";
 //comment the decorator to see null injector error.
 
 export class TasksService {
+    private loggingService = inject(LoggingService);
+
     private tasks = signal<Task[]>([]);
 
     allTasks = this.tasks.asReadonly();
@@ -19,11 +22,13 @@ export class TasksService {
             status: 'OPEN'
         }
         this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+        this.loggingService.log('ADDED TASK WITH TITLE '+ taskData.title)
     }
 
     updateTaskStatus(taskId: string,  newStatus: TaskStatus) {
         // map does an operation to all elements and returns the new array.
         // if we are  changing something we create a new task and return as return the same task object.
         this.tasks.update((oldTasks) => oldTasks.map(task => task.id === taskId ? {...task, status: newStatus} : task));
+        this.loggingService.log('CHANGE TASK STATUS '+ newStatus)
     }
 }
