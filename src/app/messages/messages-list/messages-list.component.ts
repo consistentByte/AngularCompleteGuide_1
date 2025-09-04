4b2f9475dc4f553b88f6f60092b1a9111b8b757b
Saurabh Pandey <consistentByte@gmail.com>
2025-09-04 14:28:48 +0530

    201 => The Problem With OnPush, Cross-Component Data using services & Not Using Signals

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -17,10 +17,26 @@ export class MessagesListComponent {
   // messages = input.required<string[]>();
 
   private messagesService = inject(MessagesService);
-  messages = this.messagesService.allMessages;
+  get messages() {
+    return this.messagesService.allMessages;
+  }
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
     return 'MessagesList Component Debug Output';
   }
 }
+
+/* 
+  after converting the app frfom using normal variables and getter instead of signals passed communicating via services along with OnPush,
+  On adding a new message, message list is not visible and logs not visible of message-list,
+
+  the message is not showing up down here.
+
+Indeed, if you take a look at the log messages, you will not see a log message for this messages list component. It is not part of that log because change detection was not triggered for that component.
+
+And why was that the case? Well, because the messages list component is an OnPush component, and therefore, as I explained, change detection for this component runs if an input changes. Here, I have no inputs.
+
+If we trigger it manually, that is not happening here. If we have an event in this component or a child component, that is also not the case here.
+ Or if a Signal changes here in this component, that is also not the case. And that is why this component is never checked for changes and why the new message does not show up here.
+*/
--- src/app/messages/new-message/new-message.component.ts
+++ src/app/messages/new-message/new-message.component.ts
@@ -32,7 +32,8 @@ If we do not use signals everything above will be true, but if we use signals ev
 export class NewMessageComponent {
   // add = output<string>();
   private messageService = inject(MessagesService);
-  enteredText = signal('');
+  // enteredText = signal('');
+  enteredText = '';
 
   get debugOutput() {
     console.log('[NewMessage] "debugOutput" binding re-evaluated.');
@@ -41,7 +42,7 @@ export class NewMessageComponent {
 
   onSubmit() {
     // this.add.emit(this.enteredText());
-    this.messageService.addMessage(this.enteredText());
-    this.enteredText.set('');
+    this.messageService.addMessage(this.enteredText);
+    this.enteredText ='';
   }
 }
