358e195c44e34714abe004c764d672586bc21ec3
Saurabh Pandey <consistentByte@gmail.com>
2025-09-04 13:16:13 +0530

    196 => Avoiding Zone Pollution, NgZone, runOutsideAngular function

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, NgZone, OnInit, signal } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
@@ -9,8 +9,28 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
+  private zone = inject(NgZone);
+
+
   count = signal(0);
+
+  ngOnInit(): void {
+    // all bindings will run again, as zonejs sees expired timers as a change and angular runs change detection again.
+      setTimeout(() => {
+        this.count.set(0);
+      },4000);
+    
+    // To opt out of zonejs watch mode, and skip angular change detection for some code.  
+      this.zone.runOutsideAngular(()=> {
+        //change detection will not run if this time out expires. this optimization can help us a few times. 
+        setTimeout(()=> {
+          console.log('Timer Expired');
+        }, 5000);
+
+      // This concept is also known as avoiding zone pollution because you are not polluting zone.js with events that do not matter in the end.
+      })
+  }
 
   get debugOutput() {
     console.log('[Counter] "debugOutput" binding re-evaluated.');
