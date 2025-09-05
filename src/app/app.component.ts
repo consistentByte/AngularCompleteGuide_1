b243888bc12240c288c90b31ae9109cac378cd18
Saurabh Pandey <consistentByte@gmail.com>
2025-09-05 17:32:52 +0530

    208 =>Working with rxjs operators, pipe method, map operator.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, DestroyRef, inject, OnInit } from '@angular/core';
-import {interval} from 'rxjs';
+import {interval, map} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +11,12 @@ export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
-    //after every 1000 milisecond a value will be produced
-    //In order to listen or use values that are emitted, we need to subscribe
-    //Also in order to kick off the observable we need to subscribe as internally rxjs thinks that if no one is listening, then it doesnt make sense to emit any.
-    //object of subscribe method => next: when a value is emitted, complete: if observable is completed, error : run if error occured in observable.
-    const subscription = interval(1000).subscribe({
+    // pipe method allows us to add, rxjs operators to emitted values.
+    //map operator, converts the value emitted by the observable.
+    // we can add multiple operators in pipe.
+    const subscription = interval(1000).pipe(
+      map(val => val*2),
+    ).subscribe({
       next: (val) => console.log(val),
       complete: () => console.log('completed'), //executes when completed, however this never gets completed.
       // error: () =>
