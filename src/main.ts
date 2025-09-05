18cda4d04657635d9047eccff1ac1114d0461fc1
Saurabh Pandey <consistentByte@gmail.com>
2025-09-05 13:17:46 +0530

    206 => init

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 
-bootstrapApplication(AppComponent, {
-    providers: [provideExperimentalZonelessChangeDetection()]
-}).catch((err) => console.error(err));
-
-// Inlcluding special provider for going zoneless, provideExperimentalZonelessChangeDetection
-// now zonejs wont tell angular if something changes, only signals will tell.
+bootstrapApplication(AppComponent)
+  .catch((err) => console.error(err));
