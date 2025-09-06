49ec33c37b49197d540c7ecba74b6a1ebe335747
Saurabh Pandey <consistentByte@gmail.com>
2025-09-06 09:11:25 +0530

    211 => Converting signal to observable, toObsevable. here we  attach a signal to an observable.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, computed, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 import {interval, map} from 'rxjs';
 
 @Component({
@@ -8,6 +9,7 @@ import {interval, map} from 'rxjs';
 })
 export class AppComponent implements OnInit {
   clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount);
   interval = signal(0);
   doubleInterval = computed(() => this.interval()*2);
 
@@ -15,9 +17,15 @@ export class AppComponent implements OnInit {
 
   constructor() {
     //Whenever a function in the effect callback updates it reruns,
-    effect(() => {
-      console.log(`Clicked button ${this.clickCount()} times`);
-    });
+    // effect(() => {
+    //   console.log(`Clicked button ${this.clickCount()} times`);
+    // });
+
+    //to convert a signal to observable.
+
+    // toObservable(this.clickCount);
+    
+    // can also be used in places where we inject value.
   }
 
   ngOnInit(): void {
@@ -29,18 +37,21 @@ export class AppComponent implements OnInit {
     // pipe method allows us to add, rxjs operators to emitted values.
     //map operator, converts the value emitted by the observable.
     // we can add multiple operators in pipe.
-    const subscription = interval(1000).pipe(
-      map(val => val*2),
-    ).subscribe({
-      next: (val) => console.log(val),
-      complete: () => console.log('completed'), //executes when completed, however this never gets completed.
-      // error: () =>
-    });
+    // const subscription = interval(1000).pipe(
+    //   map(val => val*2),
+    // ).subscribe({
+    //   next: (val) => console.log(val),
+    //   complete: () => console.log('completed'), //executes when completed, however this never gets completed.
+    //   // error: () =>
+    // });
+
+    const subscription = this.clickCount$.subscribe({
+      next: (val) => console.log(`Clicked button ${this.clickCount()} times`)
+    })
 
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
     })
-
   }
 
   onClick() {
@@ -60,4 +71,7 @@ Observable runs if atleast one listener, signals execute whether or not we liste
 
 Observables are great for managing events and streamed data.
 Signals great for managing application state.
+
+toObservable => we have converted the signal to an observable, or more precisely, created an extra observable based on that signal. 
+We can still update the original signal, and a new value will be emitted in the observable created with the help of toObservable.
 */
