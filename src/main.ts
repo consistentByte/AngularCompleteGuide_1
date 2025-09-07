9f1e8b0c44ddfe8f303ceb0f2eeab6c1575495b7
Saurabh Pandey <consistentByte@gmail.com>
2025-09-07 17:12:18 +0530

    235 => Introducing HTTP interceptors, pass withInterceptors function in the place where HttpClient is provided, pass and array of interceptors to it.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,45 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpHandler, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 
+// unknown => can be any type of req with any type of body, generic shows type of request body.
+function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
+    // We can use the request directly but we are cloning to produce a new one. And then doing a few changes
+    // request.headers.set => fetching all headers then also setting a new one.
+    // const req = request.clone({
+    //     headers: request.headers.set('X-DEBUG', 'TESTING')
+    // });
+    console.log('[Outgoing Request]');
+    console.log(request);
+    // pass intercepted req to next function
+    // return next(req);
+    return next(request);
+}
 
 bootstrapApplication(AppComponent, {
-    providers: [provideHttpClient()]
+    providers: [provideHttpClient(
+        withInterceptors([
+            // pass the interceptors in this array.
+            loggingInterceptor,
+        ])
+    )]
 }).catch((err) => console.error(err));
 
 
 // provideHttpClient , now HttpClient is provided in entire application
 // If not using modules, provide HttpClient service in main.ts, if using modules, then provide it in root module.
 // If using modules, provideHttpClient() is passed to 'provider' parameter of @NgModule decorator of root module.
+
+/*
+interceptors, which are special functions that will be executed when a request is about to be sent or when a response arrives.
+This means that besides the place where you triggered a request or where you subscribed to the request observable, you have another place where you can execute logic. 
+That is the idea behind interceptors.
+
+interceptor function is just another function and can be stored in any file.
+
+To register such an interceptor, you should go to the place where you provide the HTTP client.
+    pass withInterceptors function to it, in which we pass an array where we pass all the interceptor functions.
+
+interceptor function accepts two arguments: the intercepted request object and a next function, which you would call to allow the intercepted request to continue.
+*/
