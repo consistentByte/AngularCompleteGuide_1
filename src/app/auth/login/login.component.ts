24315c8c1b9221ca3ba3c668ea77092bbe2b0eb8
Saurabh Pandey <consistentByte@gmail.com>
2025-09-09 20:02:36 +0530

    243 => Validation inputs with form validation directives, email, required, minlength, maxlength, pattern. ngForm object status as invalid if validation checks failed.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -10,10 +10,15 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class LoginComponent {
   onSubmit(formData: NgForm) {
+    // not passing validation checks wont stop us from submitting the form, instead angular does changes to the ngForm object stating validation check failed.
+    if (formData.form.invalid) {
+      // if validation checks fail angular sets status of ngForm as invalid, and also the invalid property to true
+      return;
+    }
     console.log(formData);
     const enteredEmail = formData.form.value.email;
     const enteredPassword = formData.form.value.password;
-
+    console.log(formData.form);
     console.log(enteredEmail, enteredPassword);
   }
 }
