27c3e10f738abbbf0ddf12df21d4bf6bec378629
Saurabh Pandey <consistentByte@gmail.com>
2025-09-13 23:08:10 +0530

    256 =>  Setting up email and pass in sign up form, button type reset

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+
+  onSubmit() {
+    console.log(this.form.value.email, this.form.value.password);
+  }
+
+  onReset() {
+    // button type reset will automatically reset the form input.
+    // we can also use reset method of form for internally managed status, reset the form object. this way we dont have to make button type as reset.
+    this.form.reset();
+  }
+}
