33d615aa629b4a3fd1a40d207976253403f6d3d0
Saurabh Pandey <consistentByte@gmail.com>
2025-09-13 14:05:13 +0530

    250 => Handling form submission, Reactive form, ngSubmit.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -9,19 +9,22 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  // A form in angular is a formgroup object.
-  // we have to pass an object having key: value pair and every key value pair represents one control inside of that form group or nested form group.
-
-  //First step : Setting up form in ts file for reactive form.
   form = new FormGroup({
     email: new FormControl(''),
-    password: new FormControl(''), // can be passed with or without an initial value
+    password: new FormControl(''),
   });
-  //Second step, let angular know how our form is connected to our actual input element in html.
-
-  // This is how a form is created using reactive form approach.
 
   onSubmit() {
-    // this.form.value.email  to get the value.
+    console.log(this.form);
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.password;
+
+    console.log(enteredEmail, enteredPassword);
   }
 }
+/*
+  We get the same form group object as template driven form, because angular manages form in the same way, its just that the setup is different.
+  We get better ts support in Reactive form, as our setup lies in ts file
+  so it understands the code better.
+
+*/
