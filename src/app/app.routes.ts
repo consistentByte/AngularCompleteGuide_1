62b98cd32da59abdb5ac5096aab1830d177f5544
Saurabh Pandey <consistentByte@gmail.com>
2025-09-17 00:38:53 +0530

    274 => Working with nested routes, children in Routes, childRoutes, setting router-outlet for child Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,12 @@ export const routes: Routes = [
   {
     path: 'users/:userId', // <domain>/users/<uid>, :___ => dynamic part
     component: UserTasksComponent,
+    children: [
+      {
+        path: 'tasks', // <domain>/users/<uid>/tasks
+        component: TasksComponent,
+      },
+    ],
   },
   // {
   //   path: 'tasks', // <domain>/tasks
@@ -21,4 +27,11 @@ export const routes: Routes = [
 /*
   Routes are resolved from top to bottom, so in case of different routes, order won't matter,
   but for similar ones, it does matter.
+
+  childRoutes help us to create nested routes, using children property.
+  paths passed to children will automatically concatenated to parent path.
+
+  the router-outlet in app.comp is for main routes.
+  Child routes need a separate router-outlet in a component of which they are child of.
+  So in above case a router-outlet must be added in UserTasksComponent.
 */
