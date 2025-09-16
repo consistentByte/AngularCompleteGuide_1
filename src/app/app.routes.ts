e5edd097f15d9a949dc6fcaa0e138e816380ef2f
Saurabh Pandey <consistentByte@gmail.com>
2025-09-16 21:09:39 +0530

    267 => Registering Multiple Routes, order of routes is resolved from top-bottom, so order does matter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
+import { NoTaskComponent } from './tasks/no-task/no-task.component';
 
 export const routes: Routes = [
   {
-    path: 'tasks',
+    path: '', // <domain>
+    component: NoTaskComponent,
+  },
+  {
+    path: 'tasks', // <domain>/tasks
     component: TasksComponent,
   },
 ];
+
+/*
+  Routes are resolved from top to bottom, so in case of different routes, order won't matter,
+  but for similar ones, it does matter.
+*/
