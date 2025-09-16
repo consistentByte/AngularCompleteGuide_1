972701d886f125b270ee78d3cb8276aeae7146a8
Saurabh Pandey <consistentByte@gmail.com>
2025-09-16 23:35:53 +0530

    271 => Extracting Dynamic route parametes via Inputs, passing property with same name as path param to input and telling angular that we are using Input to fetch the dynamic route data, this is done via withComponentInputBinding

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { routes } from './app.routes';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes, withComponentInputBinding())],
 };
+
+/*
+  withComponentInputBinding: to enable enable binding approach to read dynamic values of route.
+*/
--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-user-tasks',
@@ -6,4 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent {
+  userId = input.required<string>();
+
+  private usersService = inject(UsersService);
+
+  userName = computed(
+    () => this.usersService.users.find((u) => u.id === this.userId())?.id
+  );
+}
+/*
+  Different ways of getting hold of a path parameter value:
+  1] Extracting Route params via Inputs: 
+    property name with same name as path param in routes, passed here to input.
+      passing string as generic since url is a string.
+      For this approach to work, you need to configure your Angular application router accordingly. 
+      In our application configuration, we must tell the Angular router that we want to use this input-based approach.
+      We do this by calling provideRouter and passing a second argument which is the result of calling withComponentInputBinding().
+
+      This function creates a configuration object that enables input binding for route parameters when passed as an argument to provideRouter
+      (not as the first argument, which must be the routes array).
+  
+    */
