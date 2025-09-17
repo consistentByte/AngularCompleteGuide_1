fcb135efe7c5c077af6bd046409938495401d1f8
Saurabh Pandey <consistentByte@gmail.com>
2025-09-17 22:27:04 +0530

    280 => Redirecting users, redirectTo property, pathMatch to full or prefix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,11 +9,18 @@ export const routes: Routes = [
   {
     path: '', // <domain>
     component: NoTaskComponent,
+    // redirectTo: '/users/u1',
+    // pathMatch: 'prefix' //<domain>/ =>  error as every path starts from '', so infinite redirect error, so use full instead
   },
   {
     path: 'users/:userId', // <domain>/users/<uid>, :___ => dynamic part
     component: UserTasksComponent,
     children: [
+      {
+        path: '', // <domain>/users/<uid>, adding default behavior that if no thing is added after parent path, redirect
+        redirectTo: 'tasks',
+        pathMatch: 'prefix', // when adding redirectTo property we must add this pathMatch key to (prefix or full) to tell angular how to parse the redirect path.
+      }, // putting pathMatch as prefix won't matter here, as we are dealing with nested child route here.
       {
         path: 'tasks', // <domain>/users/<uid>/tasks
         component: TasksComponent,
