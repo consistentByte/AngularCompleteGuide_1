bb22a77f2417c2c9882ab492bb26adea22b04999
Saurabh Pandey <consistentByte@gmail.com>
2025-09-17 20:51:29 +0530

    276 => Accessing Parent Route Data From Inside Nested Routes, input and activatedRoute approach can be used, but to use input approach, provide withRouterConfig and set paramsInheritanceStrategy to always

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
@@ -11,5 +11,22 @@ import { Task } from './task/task.model';
   imports: [TaskComponent],
 })
 export class TasksComponent {
+  userId = input.required<string>();
   userTasks: Task[] = [];
 }
+
+/*
+  Accessing Parent Route Data From Inside Nested Routes:
+    we can get it by both, input or Activated route method.
+
+    Activated Route approach would work straight away.
+    But input approach won't work for child routes out of the box just by 
+      user = input.required<string>();
+      as by default child routes do not receive those path parameters as input.
+      For this we have to explicitly tell angular about this by passing additional config to provideRouter,
+        withRouterConfig(),
+        In withRouterConfig we can pass a config object and set paramsInheritanceStrategy: 'always'
+      After this the dynamic path param values are injected into child routes.
+
+
+*/
